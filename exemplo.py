fa7ff629145651a67b22fee8178505560b7b0554
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2020-05-04 00:20:58 -0300

    texto centraliza bem e a fonte se adapta ao tamanho da imagem

--- exemplo.py
+++ exemplo.py
@@ -24,20 +24,27 @@ def main():
     # new_image.setSize(width=int(1024*9/19), paddingX=0.1, paddingY=0.1)
 
     # define o esquema de cores da imagem
-    # new_image.setColorScheme("insper-talus")
+    # new_image.setColorScheme("terminal-red")
 
-    string1 = "exemplo de texto com seis palavras"
+    # desenha as "seleção" do texto
+    new_image.drawSelection = True
+
+    string1 = """
+    Programa que gera imagens com base em textos. O texto obedece um template a ser definido na hora da criação;
+O texto é centralizado e o programa adapta o tamanho da fonte caso exista tendência de extrapolar os limites;
+Para conhecer os limites, pode-se ativar o 'modo debug', que já possui funções que ajudam na hora de encontrar erros.
+    """
     # define as caracteristicas do texto principal da imagem
     new_image.setText(string1)
-    new_image.setTextFont("ubuntu-bold",45)
+    new_image.setTextFont("ubuntu-bold", 45)
 
     # define as caracteristicas do titulo
-    new_image.setTitleFont("ubuntu-regular", 30)
-    new_image.setTitle("Machine Learning (1/2)")
+    new_image.setTitleFont("ubuntu-regular", 40)
+    new_image.setTitle(f"(debug_mode = {new_image.debug})")
 
     # define as caracteristicas dos créditos
-    new_image.setCreditsFont("ubuntu-regular", 30)
-    new_image.setCredits("@talus_insper")
+    new_image.setCreditsFont("firacode-bold", 40)
+    new_image.setCredits("@ImageGenerator")
 
     # exibe a imagem em modo de desenvolvimento
     new_image.process(show=True)
